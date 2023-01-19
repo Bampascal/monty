6b069b4b16c1de3d245db26688f2c010ba91f520
Bampascal <akinolabamemmanuel.com>
2023-01-19 13:19:50 -0800

    monty

--- monty_operators2.c
+++ monty_operators2.c
@@ -19,4 +19,21 @@ void f_push(stack_t **head, unsigned int counter)
 		{
 			if (bus.arg[m] > 57 || bus.arg[m] < 48)
 				flag = 1; }
-
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	i = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, i);
+	else
+		addqueue(head, i);
+}
